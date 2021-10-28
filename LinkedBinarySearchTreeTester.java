e8bc940c5d9f4dbf36410c2e049463e06838feb5
Baochuan Lu <lubaochuan@gmail.com>
2021-10-28 12:49:38 -0500

    fix test case

--- LinkedBinarySearchTreeTester.java
+++ LinkedBinarySearchTreeTester.java
@@ -58,8 +58,8 @@ public class LinkedBinarySearchTreeTester{
     System.out.println("expect:\t4");
     System.out.println("got:\t"+tree.find(4));
 
-    System.out.println("test: find(8)");
-    System.out.println("expect:\t8");
-    System.out.println("got:\t"+tree.find(8));
+    System.out.println("test: find(9)");
+    System.out.println("expect:\tnull");
+    System.out.println("got:\t"+tree.find(9));
   }
 }
